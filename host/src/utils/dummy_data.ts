afd71c15c48457d613e542cbdeada83190689d33
Josh Graber <jgraber@covermymeds.com>
2019-08-28 08:59:44 -0400

    Adjust XSS scenario slightly

--- host/src/utils/dummy_data.ts
+++
@@ -1,8 +0,0 @@
-export function getDummyResults(query: string) {
-  return [
-    '* pizza',
-    'Jonathan *',
-    'super *',
-    '* crazy',
-  ].map(str => str.replace('*', query))
-}
--- host/src/web/xss.ts
+++ host/src/web/xss.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import { logGet } from '../utils/log'
-import { getDummyResults } from '../utils/dummy_data'
 
 const router = express.Router()
 
@@ -8,13 +7,10 @@ router.get('/search', (req, res) => {
   logGet('/xss/search', req)
 
   const query = req.param('query')
-  const results = !!query 
-    ? getDummyResults(query)
-    : []
 
   // Turn off browser XSS protection for the sake of this demonstration
   res.header('X-XSS-Protection', '0')
-  res.render('xss/search', { query, results })
+  res.render('xss/search', { query, results: [] })
 })
 
 export default router
