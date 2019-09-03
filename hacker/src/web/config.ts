6246945f47eef53862f2dd09d44afe5224751e64
Josh Graber <jgraber@covermymeds.com>
2019-09-03 10:26:58 -0400

    Ensure hacker server accepts CORS
    Hacker wants cross origin requests so it can receive data from targets.

--- hacker/src/web/config.ts
+++ hacker/src/web/config.ts
@@ -1,15 +1,21 @@
 import * as express  from 'express'
 import { Express } from 'express'
+import cors from 'cors'
 import * as bodyParser from 'body-parser'
 
 export default function configure(app: Express) {
   _serveAssets(app)
+  _cors(app)
   _initBodyParser(app)
   _initViewRendering(app)
 }
 
 function _serveAssets(app: Express) {
   app.use(express.static('assets'))
+}
+
+function _cors(app: Express) {
+  app.use(cors())
 }
 
 function _initBodyParser(app: Express): void {
